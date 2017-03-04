f1e827ccad0c800ee70a23d67535b77d3ec4b871
Sean Harrison <sah@blackearth.us>
2017-03-04 16:41:22 -0600

    DOCX now supports the Source interface, including returning Image objects (with image data!) for all images in the DOCX.

--- bkgen/docx.py
+++ bkgen/docx.py
@@ -1,19 +1,12 @@
 
 import os
 from lxml import etree
+from bl.url import URL
 import bxml.docx
 from bkgen import NS, Source
 
 class DOCX(bxml.docx.DOCX, Source):
     """A Word document can be a source that is brought in, and an output format"""
-
-    def documents(self, path=None):
-        """return a list of documents containing the content of the document"""
-        # just the one document
-        fn = os.path.join(
-                path or os.path.dirname(os.path.abspath(self.fn)),
-                os.path.basename(os.path.splitext(self.fn)[0]+'.xml'))
-        return [self.document(fn=fn)]
 
     def document(self, fn=None): 
         """returns an XML document containing the content of the Word document"""
@@ -22,16 +15,41 @@ class DOCX(bxml.docx.DOCX, Source):
         doc = converter.convert(self, fn=fn or os.path.splitext(self.fn)[0]+'.xml')
         return doc
 
-    def resources(self, path=None):
-        """return a list of files representing the resources in the document"""
-        # currently an empty list
-        return []
+    @property
+    def documents(self):
+        """return a list of documents containing the content of the document"""
+        # just the one document
+        return [self.document()]
 
+    @property
+    def images(self):
+        """all the images referred to in the DOCX. 
+        """
+        from bf.image import Image
+        images = []
+        rels = self.xml(src='word/_rels/document.xml.rels').root
+        for img in root.xpath("//html:img", namespaces=DOCX.NS):
+            image = Image()
+            link_rel = XML.find(rels, "//rels:Relationship[@Id='%s']" % img.get('data-link-id'), namespaces=DOCX.NS)
+            embed_rel = XML.find(rels, "//rels:Relationship[@Id='%s']" % img.get('data-embed-id'), namespaces=DOCX.NS)
+            if link_rel is not None:
+                image.fn = URL(link_rel.get('Target')).path
+                if embed_rel is not None:
+                    image.data = self.read('word/' + embed_rel.get('Target'))
+                    image.fn = os.path.join(self.path, img.attrib.pop('name'))
+                else:
+                    image.data = open(image.fn, 'rb').read()
+            images.append(image)
+        return images
+
+    @property
     def metadata(self):
-        """return an opf:metadata element with the metadata in the document"""
-        xml = self.xml(src="docProps/core.xml")
+        """return a Metadata object with the metadata in the document"""
+        from .metadata import Metadata
+        xml = self.xml(src="docProps/core.xml", XMLClass=Metadata)
         xml.root.tag = "{%(pub)s}metadata" % NS
-        return xml.root
+        return xml
 
-
-
+    @property
+    def stylesheet(self):
+        return super().stylesheet()
