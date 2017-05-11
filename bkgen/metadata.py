2dc1a6362022f54311932d76ca6a5c67fbbb70b1
Sean Harrison <sah@blackearth.us>
2017-05-11 17:31:55 -0500

    Most metadata properties now exposed as Metadata @properties

--- bkgen/metadata.py
+++ bkgen/metadata.py
@@ -10,23 +10,62 @@ class Metadata(XML):
         for pattern in id_patterns:
             identifier = self.find(self.root, "dc:identifier[contains(@id, '%s')]" % pattern, namespaces=NS)
             if identifier is not None: return identifier
- 
+
+    def element_text(self, xpath):
+        return self.find(self.root, "%s/text()" % xpath, namespaces=NS)
+
+    def meta_text(self, property):
+        return self.element_text("opf:meta[@property='%s']/text()" % property)
+
     @property
     def title_text(self):
-        return self.find(self.root, "dc:title/text()", namespaces=NS)
+        return self.element_text("dc:title")
+
+    @property
+    def publisher_text(self):
+        return self.element_text("dc:publisher")
+
+    @property
+    def date_text(self):
+        return self.element_text("dc:date")        
 
     @property
     def description_text(self):
-        return self.find(self.root, "dc:description/text()", namespaces=NS)
+        return self.element_text("dc:description")
 
-    def meta_text(self, property):
-        return self.find(self.root, "opf:meta[@property='%s']/text()" % property, namespaces=NS)
+    @property
+    def rights(self):
+        return self.element_text("dc:rights")
+
+    @property
+    def identifiers(self):
+        return self.xpath(self.root, "dc:identifiers/text()", namespaces=NS)
+
+    @property
+    def identifiers_with_formats(self):
+        entries = []
+        for elem in self.xpath(self.root, "dc:identifier", namespaces=NS):
+            entries.append(
+                (elem.text or '', self.element_text("opf:meta[@refines='#%s']" % elem.get('id'))))
+        return entries
 
     @property
     def creators(self):
         return self.xpath(self.root, "dc:creator/text()", namespaces=NS)
 
     @property
+    def creators_with_roles(self):
+        entries = []
+        for elem in self.xpath(self.root, "dc:creator", namespaces=NS):
+            entries.append(
+                (elem.text or '', self.element_text("opf:meta[@refines='#%s']" % elem.get('id'))))
+        return entries
+
+    @property
     def contributors(self):
         return self.xpath(self.root, "dc:contributor/text()", namespaces=NS)
+
+    @property
+    def subjects(self):
+        return self.xpath(self.root, "dc:subject/text()", namespaces=NS)
 
