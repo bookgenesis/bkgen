0ed982285c76dd0cf7886f359a63a99c9d08f30e
Sean Harrison <sah@blackearth.us>
2017-05-27 19:21:09 -0500

    updated __config__.ini.TEMPLATE and include copying it to __config__.ini on first install

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
 config = {
-  "name": "bkgen",
-  "version": "0.0.1",
-  "description": "Core functionality for bookgenesis",
-  "url": "https://gitlab.com/BlackEarth/bkgen",
-  "author": "Sean Harrison",
-  "author_email": "sah@blackearth.us",
-  "license": "All rights reserved.",
-  "classifiers": [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3"
-  ],
-  "entry_points": {},
-  "install_requires": ["bl", "bf", "bxml", "bsql"],
-  "extras_require": {
-    "dev": [],
-    "test": []
-  },
-  "package_data": {
-    "": []
+    "name": "bkgen",
+    "version": "0.0.1",
+    "description": "Core functionality for bookgenesis",
+    "url": "https://gitlab.com/BlackEarth/bkgen",
+    "author": "Sean Harrison",
+    "author_email": "sah@blackearth.us",
+    "license": "All rights reserved.",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+    "entry_points": {},
+    "install_requires": ["bl", "bf", "bxml", "bsql"],
+    "extras_require": {
+        "dev": [],
+        "test": []
     },
-  "data_files": [],
-  "scripts": []
+    "package_data": {
+        "": []
+        },
+    "data_files": [],
+    "scripts": []
 }
 
-import os, json
+import os, json, shutil
 from setuptools import setup, find_packages
 from codecs import open
+
+PATH = os.path.abspath(__file__)
+configfn = os.path.join(PATH, 'bkgen', '__config__.ini')
+if not os.path.exists(configfn):
+    shutil.copy(configfn + '.TEMPLATE', configfn)
 
 setup(
     long_description="This package contains the core BookGenesis software.",
