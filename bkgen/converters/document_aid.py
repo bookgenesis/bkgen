7597b9fb4bc29eb4167772f08f988c1b76cf30e1
Sean Harrison <sah@blackearth.us>
2017-07-19 15:53:17 -0500

    deal with paragraph returns: Do whitespace-sensitive XML. also deal with special characters (but turned off for the time being)

--- bkgen/converters/document_aid.py
+++ bkgen/converters/document_aid.py
@@ -1,16 +1,16 @@
 """convert pub:document and document fragments to (X)HTML"""
 
 import logging
-log = logging.getLogger(__name__)
 
-import os
+import os, re
+from glob import glob
 from lxml import etree
 from bxml.xt import XT
 from bxml.builder import Builder
 from bl.file import File
 from bxml.xml import XML
 
-from bkgen import NS
+from bkgen import NS, config
 from bkgen.converters import Converter
 from bkgen.document import Document
 
@@ -19,25 +19,31 @@ H = Builder.single(NS.html)
 transformer = XT()
 transformer_XSLT = etree.XSLT(etree.parse(os.path.splitext(__file__)[0] + '.xsl'))
 
+log = logging.getLogger(__name__)
+logging.basicConfig(**config.Logging)
+
 class DocumentAid(Converter):
     def convert(self, document, **params):
-        return document.transform(transformer, **params)
+        doc = document.transform(transformer, **params)
+        return doc
 
 # == DEFAULT == 
 # do XSLT on the element and return the results
 @transformer.match("True")
 def default(elem, **params):
-    e = get_includes(elem, **params)
-    e = transformer_XSLT(e).getroot()
-    e = image_hrefs(e, **params)
-    return [ e ]
+    root = get_includes(elem, **params)
+    root = transformer_XSLT(root).getroot()
+    root = image_hrefs(root, **params)
+    root = paragraph_returns(root, **params)
+    # root = special_characters(root, **params)
+    return [ root ]
 
 def get_includes(root, **params):
     for incl in root.xpath(".//pub:include", namespaces=NS):
         for ch in incl: 
             incl.remove(ch)
         srcfn = os.path.join(os.path.dirname(params['fn']), incl.get('src').split('#')[0])
-        log.info(srcfn)
+        log.debug(srcfn)
         assert os.path.exists(srcfn)
         src = XML(fn=srcfn)
         if '#' in incl.get('src'):
@@ -51,8 +57,51 @@ def get_includes(root, **params):
             incl.append(elem)
     return root
 
+def special_characters(root, **params):
+    """tag special characters with <pub:x*>...</pub:x*> so that they can be rendered correctly in InDesign"""
+    for elem in root.xpath("//*[text()]"):
+        elem.text = ((elem.text or '')
+            .replace('\u00A0', '[pub:x00A0]\u00A0[/pub:x00A0]')
+            .replace('\u00AD', '[pub:x00AD]\u00AD[/pub:x00AD]')
+            .replace('\u2002', '[pub:x2002]\u2002[/pub:x2002]')
+            .replace('\u2003', '[pub:x2003]\u2003[/pub:x2003]')
+            .replace('\u2007', '[pub:x2007]\u2007[/pub:x2007]')
+            .replace('\u2008', '[pub:x2008]\u2008[/pub:x2008]')
+            .replace('\u2009', '[pub:x2009]\u2009[/pub:x2009]')
+            .replace('\u200A', '[pub:x200A]\u200A[/pub:x200A]')
+            .replace('\u2011', '[pub:x2011]\u2011[/pub:x2011]')
+            .replace('\u202F', '[pub:x202F]\u202F[/pub:x202F]')
+        )
+        elem.tail = ((elem.tail or '')
+            .replace('\u00A0', '[pub:x00A0]\u00A0[/pub:x00A0]')
+            .replace('\u00AD', '[pub:x00AD]\u00AD[/pub:x00AD]')
+            .replace('\u2002', '[pub:x2002]\u2002[/pub:x2002]')
+            .replace('\u2003', '[pub:x2003]\u2003[/pub:x2003]')
+            .replace('\u2007', '[pub:x2007]\u2007[/pub:x2007]')
+            .replace('\u2008', '[pub:x2008]\u2008[/pub:x2008]')
+            .replace('\u2009', '[pub:x2009]\u2009[/pub:x2009]')
+            .replace('\u200A', '[pub:x200A]\u200A[/pub:x200A]')
+            .replace('\u2011', '[pub:x2011]\u2011[/pub:x2011]')
+            .replace('\u202F', '[pub:x202F]\u202F[/pub:x202F]')
+        )
+    root = etree.fromstring(re.sub("\[(/?pub:[^\]]*?)\]", r"<\1>", etree.tounicode(root)))
+    return root
+
 def image_hrefs(root, **params):
     for img in root.xpath("//html:img", namespaces=NS):
         img.set('href', 'file://'+os.path.abspath(os.path.join(os.path.dirname(params['fn']), img.get('src'))))
     return root
 
+def paragraph_returns(root, **params):
+    """Put a paragraph return at the end of every paragraph/heading that is not in a table and has following content.
+    The paragraph return goes at the end of the last text in the paragraph, in case there is a span or other element
+    at the end of the paragraph (which would cause InDesign to ignore the paragraph return if it were after that element).
+    """
+    t = etree.tounicode(root).strip()
+    t = re.sub('\s*\n\s*', '', t)
+    root = etree.fromstring(t)
+    pp = root.xpath("//html:p[not(ancestor::html:table) and following::html:*]", namespaces=NS)
+    log.debug('%d paragraphs in %r' % (len(pp), root.tag))
+    for p in pp:
+        p.tail = '\n'
+    return root
