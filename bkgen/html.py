6bc832fff6a447618e68640a4892f07ad295808f
Sean Harrison <sah@blackearth.us>
2017-08-15 23:55:43 -0500

    HTML source methods HTML.css_template() provides an empty CSS with all the HTML tags and classes that occur in the HTML.

--- bkgen/html.py
+++ bkgen/html.py
@@ -1,20 +1,22 @@
 
+import os
 from bxml import XML
 from bkgen import NS
+from bkgen.source import Source
 
-class HTML(XML):
+class HTML(XML, Source):
     ROOT_TAG = "{%(html)s}html" % NS
 
-    def document(self, fn=None):
+    def document(self, fn=None, **params):
         """convert an xhtml file into a pub:document"""
         from .converters.html_document import HtmlDocument
         from .document import Document
         converter = HtmlDocument()
-        doc = converter.convert(self, fn=fn or os.path.splitext(self.fn)[0]+'.xml')
+        doc = converter.convert(self, fn=fn or os.path.splitext(self.fn)[0]+'.xml', **params)
         return doc
 
-    def documents(self):
-        return [self.document()]
+    def documents(self, **params):
+        return [self.document(**params)]
 
     def images(self):
         return []
@@ -26,7 +28,17 @@ class HTML(XML):
         return m
 
     def stylesheet(self):
+        return self.css_template()
+
+    def css_template(self):
         from .css import CSS
-        css = CSS()
-        css.fn = os.path.splitext(self.fn)[0]+'.css'
+        from bf.styles import Styles
+        styles = Styles()
+        tags = self.tag_dict(include_attribs=['class'])
+        for tag in [tag for tag in tags if NS.html in tag]:
+            tagname = XML.tag_name(tag)
+            styles[tagname] = {}
+            for c in (tags[tag].get('class') or []):
+                styles[tagname+'.'+c] = {}
+        css = CSS(fn=os.path.splitext(self.fn)[0]+'.css', styles=styles)
         return css
