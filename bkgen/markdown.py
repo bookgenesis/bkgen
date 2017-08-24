2e0bd688fbb43723dfc5e278d9177cbf8576b5bc
Sean Harrison <sah@blackearth.us>
2017-08-24 14:31:46 -0500

    Markdown extensions activated

--- bkgen/markdown.py
+++ bkgen/markdown.py
@@ -1,6 +1,7 @@
 
 import os, tempfile
 from lxml import etree
+import markdown.extensions.wikilinks
 from bl.text import Text
 from bxml.builder import Builder
 from bkgen import NS
@@ -10,6 +11,19 @@ B = Builder(default=NS.html, **{'html':NS.html,'pub':NS.pub})
 
 class Markdown(Text, Source):
 
+    EXTENSIONS = [
+        'markdown.extensions.extra',
+        'markdown.extensions.admonition',
+        # 'markdown.extensions.codehilite',
+        'markdown.extensions.headerid',
+        # 'markdown.extensions.meta',
+        'markdown.extensions.nl2br',
+        'markdown.extensions.sane_lists',
+        # 'markdown.extensions.smarty',
+        'markdown.extensions.toc',
+        markdown.extensions.wikilinks.WikiLinkExtension(base_url='', end_url='.html'),
+    ]
+
     def document(self, **params):
         """convert a markdown file into a pub:document via HTML"""
         doc = self.html().document()
@@ -18,10 +32,10 @@ class Markdown(Text, Source):
     def documents(self, **params):
         return [self.document(**params)]
 
-    def html(self, reload=False):        
+    def html(self, reload=False, output_format='xhtml5', lazy_ol=False, extensions=None):        
         if reload==True or self.__HTML is None:
             from markdown import markdown
-            content = markdown(self.text, output_format='xhtml5', lazy_ol=False)
+            content = markdown(self.text, output_format=output_format, lazy_ol=lazy_ol, extensions=extensions or self.EXTENSIONS)
             body = etree.fromstring("""<body xmlns="%s">\n%s\n</body>""" % (NS.html, content))
             root = B.html.html('\n\t', body, '\n')
             for e in root.xpath("//*[contains(@href, '.md')]"):
