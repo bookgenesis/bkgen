2a158637952b42d1767cd389e6f9c1f20231b089
Sean Harrison <sah@blackearth.us>
2017-09-09 22:17:41 -0500

    reworked Metadata properties to return elements, using the new XML.element()

--- bkgen/metadata.py
+++ bkgen/metadata.py
@@ -1,71 +1,71 @@
 
+from bl.dict import Dict
+from lxml import etree
 from bxml import XML
 from . import NS    
 
 class Metadata(XML):
-    ROOT_TAG = "{%(pub)s}metadata" % NS
+    ROOT_TAG = "{%(opf)s}metadata" % NS
+    NS = Dict(**{k:NS[k] for k in NS if k in ['opf', 'dc', 'dcterms', 'dcmitype', 'cp', 'xsi',]})
 
     def identifier(self, id_patterns=['isbn']):
         identifier = None
         for pattern in id_patterns:
-            identifier = self.find(self.root, "dc:identifier[contains(@id, '%s')]" % pattern, namespaces=NS)
+            identifier = self.find(self.root, "dc:identifier[contains(@id, '%s')]" % pattern)
             if identifier is not None: return identifier
 
-    def element_text(self, xpath):
-        return self.find(self.root, "%s/text()" % xpath, namespaces=NS)
-
-    def meta_text(self, property_name):
-        return self.element_text("opf:meta[@property='%s']" % property_name)
+    def meta(self, property_name):
+        return self.element("opf:meta", property=property_name)
 
     @property
-    def title_text(self):
-        return self.element_text("dc:title")
+    def title(self):
+        return self.element("dc:title")
 
     @property
-    def publisher_text(self):
-        return self.element_text("dc:publisher")
+    def publisher(self):
+        return self.element("dc:publisher")
 
     @property
-    def date_text(self):
-        return self.element_text("dc:date")        
+    def date(self):
+        return self.element("dc:date")        
 
     @property
-    def description_text(self):
-        return self.element_text("dc:description")
+    def description(self):
+        return self.element("dc:description")
 
     @property
     def rights(self):
-        return self.element_text("dc:rights")
+        return self.element("dc:rights")
 
     @property
     def identifiers(self):
-        return self.xpath(self.root, "dc:identifiers/text()", namespaces=NS)
+        return self.xpath(self.root, "dc:identifier")
 
     @property
     def identifiers_with_formats(self):
         entries = []
-        for elem in self.xpath(self.root, "dc:identifier", namespaces=NS):
+        for elem in self.xpath(self.root, "dc:identifier"):
             entries.append(
-                (elem.text or '', self.element_text("opf:meta[@refines='#%s']" % elem.get('id'))))
+                (elem.text or '', self.element("opf:meta", refines='#%s' % elem.get('id')).text or ''))
         return entries
 
     @property
     def creators(self):
-        return self.xpath(self.root, "dc:creator/text()", namespaces=NS)
+        return self.xpath(self.root, "dc:creator")
 
     @property
     def creators_with_roles(self):
         entries = []
-        for elem in self.xpath(self.root, "dc:creator", namespaces=NS):
+        for elem in self.xpath(self.root, "dc:creator"):
             entries.append(
                 (elem.text or '', self.element_text("opf:meta[@refines='#%s']" % elem.get('id'))))
         return entries
 
     @property
     def contributors(self):
-        return self.xpath(self.root, "dc:contributor/text()", namespaces=NS)
+        return self.xpath(self.root, "dc:contributor/text()")
 
     @property
     def subjects(self):
-        return self.xpath(self.root, "dc:subject/text()", namespaces=NS)
+        return self.xpath(self.root, "dc:subject/text()")
 
