8a0a72beb1e53cdd997143d512e7cfcec4d585d8
Sean Harrison <sah@bookgenesis.com>
2019-09-27 00:09:16 -0500

    remove homemade installation tools

--- INSTALL.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "Setting up the Python environment (requires Python 3)"
-cd %~dp0
-pip3 install virtualenv
-start /b /wait cmd /C "python3 -m virtualenv _bkgen"
-UPDATE.bat
-echo "Installation complete." 
