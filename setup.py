529066600d873ba800f350a324336baedb98515d
Sean Harrison <sah@kruxia.com>
2023-06-03 15:16:52 -0500

    updated install bf to get CSS

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+import os, shutil
+from setuptools import setup, find_packages
+
+# GITLAB_TOKEN = os.getenv('GITLAB_TOKEN')
+
 config = {
     "name": "bkgen",
     "version": "0.15.0",
@@ -12,7 +17,19 @@ config = {
         "Programming Language :: Python :: 3"
     ],
     "entry_points": {},
-    "install_requires": ["bl", "bf", "bxml", "pycountry"],
+    "install_requires": [
+        "bl @ git+https://github.com/BlackEarth/bl.git@40e61c905cdec2b3539cfd38ced8df76367ea2d6#egg=bl",
+        "bxml @ git+https://github.com/BlackEarth/bxml.git@dccf47e70af0c780f28479685f67f3b543b3f3fd#egg=bxml",
+        # "bf @ git+https://seanharrison:${GITLAB_TOKEN}@gitlab.com/bookgenesis/kaplan/bf.git@07746346a214f75c9857d279c0f9e5ebab92741c#egg=bf",
+        "cssselect~=1.1.0",
+        "cssutils~=1.0.2",
+        "libsass~=0.19.4",
+        "lxml~=4.9.2",
+        "Markdown~=3.1.1",
+        "pycountry~=19.8.18",
+        "six~=1.13.0",
+        "Unum~=4.1.4",
+    ],
     "extras_require": {
         "dev": [],
         "test": []
@@ -23,10 +40,6 @@ config = {
     "data_files": [],
     "scripts": []
 }
-
-import os, json, shutil
-from setuptools import setup, find_packages
-from codecs import open
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 configfn = os.path.join(PATH, 'bkgen', '__config__.ini')
